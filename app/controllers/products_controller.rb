a6f8234fe7be715eeb3e74684922397131cc18ad
Nikki Veen <nikkiveen@gmail.com>
2016-03-10 19:08:27 -0600

    created route, method, html/form for creating new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,14 @@ class ProductsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    render 'index.html.erb'
+  end
+
   def show
     product_id = params[:id]
     @product = Product.find_by(id: product_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post 'products' => 'products#create'
   get '/products/:id' => 'products#show'
 end
