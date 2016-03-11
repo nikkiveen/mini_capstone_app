16207d1432c1b39260a4d7cd57820c22dd4c9286
Nikki Veen <nikkiveen@gmail.com>
2016-03-10 21:34:26 -0600

    created update and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,4 +30,25 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: product_id)
     render 'edit.html.erb'
   end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    
+    @product.update(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image: params[:image]
+    )
+
+    render 'update.html.erb'
+  end
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    render 'destroy.html.erb'
+  end
 end
