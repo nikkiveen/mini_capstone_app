942327dcadb5c5b7865366e4f1a7f5da42f68f8d
Nikki Veen <nikkiveen@gmail.com>
2016-03-13 14:10:40 -0500

    added redirects and flash messages to create, update, and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,14 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(
+    product = Product.create(
       name: params[:name],
       description: params[:description],
       price: params[:price],
       image: params[:image]
     )
-
-    render 'create.html.erb'
+    flash[:success] = "Product has been successfully created!"    
+    redirect_to "/products/#{product.id}"
   end
 
   def show
@@ -41,14 +41,16 @@ class ProductsController < ApplicationController
       price: params[:price],
       image: params[:image]
     )
-
-    render 'update.html.erb'
+    
+    flash[:success] = "Product has been successfully edited!"
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
     product_id = params[:id]
     @product = Product.find_by(id: product_id)
     @product.destroy
-    render 'destroy.html.erb'
+    flash[:success] = "Product has been successfully deleted!"
+    redirect_to '/products'
   end
 end
