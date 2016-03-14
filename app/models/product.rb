4e5024fa4afc07ec6758259ef21c3d9150c14d6d
Nikki Veen <nikkiveen@gmail.com>
2016-03-14 18:54:19 -0500

    created model methods for sales message, sales tax, and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_i < 2
+      @message = "Clearance Item!"
+    else
+      @message = "On Sale!"
+    end
+  end
+
+  def tax
+    @sales_tax = price.to_i * 0.09
+  end
+
+  def total
+    @sales_tax = price.to_i * 1.09
+  end
 end
