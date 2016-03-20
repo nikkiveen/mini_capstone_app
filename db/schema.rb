2409c30ae3d47e3f20d3f83b7e652f9dea997bbf
Nikki Veen <nikkiveen@gmail.com>
2016-03-20 14:23:17 -0500

    added user id column to products table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320162256) do
+ActiveRecord::Schema.define(version: 20160320191633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160320162256) do
     t.datetime "updated_at",                                         null: false
     t.boolean  "stock",                               default: true
     t.integer  "supplier_id"
+    t.integer  "user_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
