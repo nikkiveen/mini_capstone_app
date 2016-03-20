31d94808a5f22d3e8bd0b9d45b3ba9e4df2173d1
Nikki Veen <nikkiveen@gmail.com>
2016-03-20 15:53:22 -0500

    updated create method to make a new order in the database

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
   def create
+    Order.create(
+      quantity: params[:quantity],
+      user_id: current_user
+    ) 
+    
     render 'create.html.erb'
   end
 end
