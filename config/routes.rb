8b4938e2ccd8b4d43e1d678e75e62338310e0f5f
Nikki Veen <nikkiveen@gmail.com>
2016-03-23 21:20:04 -0500

    updated order form on product show page to route to carted products controller

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
   ##########################################################
   post '/orders' => 'orders#create'
   get '/orders/:id' => 'orders#show'
+  ##########################################################
+  post '/carted_products' => 'carted_products#create'  
 end
