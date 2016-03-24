a512c6432e18cb1a69490e524703a6189faf64b7
Nikki Veen <nikkiveen@gmail.com>
2016-03-23 21:04:07 -0500

    removed product id and quantity from orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324015913) do
+ActiveRecord::Schema.define(version: 20160324020257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 20160324015913) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "product_id"
-    t.integer  "quantity"
     t.decimal  "subtotal",   precision: 7, scale: 2
     t.decimal  "tax",        precision: 7, scale: 2
     t.decimal  "total",      precision: 7, scale: 2
