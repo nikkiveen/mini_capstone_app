2d0542bad11164b909442d2da11018072af2baa6
Nikki Veen <nikkiveen@gmail.com>
2016-03-24 20:32:44 -0500

    redirected user to home page if no products in cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -2,7 +2,9 @@ class CartedProductsController < ApplicationController
   def index
     @carted_products = CartedProduct.where("user_id = ? AND status = ?", current_user.id.to_i, "Carted")
 
-    render 'index.html.erb'
+    if CartedProduct.where(status: 'Carted').count == 0
+      redirect_to '/products'
+    end
   end
 
   def create
@@ -15,6 +17,4 @@ class CartedProductsController < ApplicationController
 
     redirect_to '/carted_products'
   end
-
-
 end
